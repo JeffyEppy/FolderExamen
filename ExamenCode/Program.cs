c911f0dcd713bc7c2a95c09ed1eedc952714ce80
JeffyEppy <matteodebelser2@gmail.com>
2026-06-15 09:21:09 +0200

    CommitLimiet

--- ExamenCode/Program.cs
+++ ExamenCode/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var movie = new Movie("Inception", "Science Fiction", 148);
+var movie = new Movie("Inception", "Science Fiction", 148, 13);
 movie.ShowInfo();
 
 public class Movie
@@ -8,12 +8,14 @@ public class Movie
     public string Title { get; }
     public string Genre { get; }
     public int DurationInMinutes { get; }
+    public int AgeLimit { get; }
 
-    public Movie(string title, string genre, int durationInMinutes)
+    public Movie(string title, string genre, int durationInMinutes, int ageLimit)
     {
         Title = title;
         Genre = genre;
         DurationInMinutes = durationInMinutes;
+        AgeLimit = ageLimit;
     }
 
     public void ShowInfo()
@@ -21,5 +23,6 @@ public class Movie
         Console.WriteLine($"Title: {Title}");
         Console.WriteLine($"Genre: {Genre}");
         Console.WriteLine($"Duration: {DurationInMinutes} minutes");
+        Console.WriteLine($"Age Limit: {AgeLimit}+");
     }
 }
