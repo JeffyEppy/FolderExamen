c64078b051c0f92d3516d8e60385b602f94d25f9
JeffyEppy <matteodebelser2@gmail.com>
2026-06-15 09:15:14 +0200

    movie1

--- ExamenCode/obj/Debug/net9.0/ExamenCode.AssemblyInfo.cs
+++ ExamenCode/obj/Debug/net9.0/ExamenCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExamenCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bfb883290ee0e57fc14ce54487ffdc229f906ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExamenCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExamenCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
