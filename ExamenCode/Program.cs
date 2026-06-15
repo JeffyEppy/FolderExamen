7bfb883290ee0e57fc14ce54487ffdc229f906ad
JeffyEppy <matteodebelser2@gmail.com>
2026-06-15 09:12:56 +0200

    movie

--- ExamenCode/Program.cs
+++ ExamenCode/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+var movie = new Movie("Inception", "Science Fiction", 148);
+movie.ShowInfo();
+
+public class Movie
+{
+    public string Title { get; }
+    public string Genre { get; }
+    public int DurationInMinutes { get; }
+
+    public Movie(string title, string genre, int durationInMinutes)
+    {
+        Title = title;
+        Genre = genre;
+        DurationInMinutes = durationInMinutes;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Genre: {Genre}");
+        Console.WriteLine($"Duration: {DurationInMinutes} minutes");
+    }
+}
